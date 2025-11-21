b94095d018146141f1ac8df94f7bb58b3a22cc8b
hwwv <1355394511@qq.com>
2025-11-20 23:23:36 -0500

    Task 1.1 finish the style problem

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,19 +1,38 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ * Neither name nor type should be null.
+ */
 public class Play {
 
     private final String name;
     private final String type;
+
+    /**
+     * Constructs a play.
+     *
+     * @param name the name of the play; not null.
+     * @param type the type of the play; not null.
+     */
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
 
+    /**
+     * It is getting the name of the play.
+     * @return the name of the play, never null.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * It is getting the type of the play.
+     * @return the type of the play; never null.
+     */
     public String getType() {
         return type;
     }
